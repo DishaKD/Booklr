a9f42101e75bff42ca7cd5055631ca726f16459a
Dishal Kanishka <87950341+Dotax14@users.noreply.github.com>
2023-06-10 00:58:46 +0530

    Initial Commit

---
+++ Home-Page.php
@@ -0,0 +1,104 @@
+<!--Home Page-->
+<!DOCTYPE html></Doctype>
+<html lang=en>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale-1">
+
+
+    <Head>
+        <link rel="stylesheet" href="./Styles/Home_Page - Style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+        <title>Booklr</title>
+    </Head>
+
+    <!--Header-->
+    <Header class="page header">
+        <!--Logo-->  
+        <img class="header_Logo"src="" alt="Booklr">
+        
+        <!--Search Bar-->
+        <div class="boxContrainer">
+            <table class="elementsContrainer">
+                <tr>
+                    <td>
+                        <input type="text" placeholder="Search Book" class="search">
+                    </td>
+                    <td>
+                        <a href="#"><i class="material-icons">search</i></a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <!--Navigation Bar-->        
+        <div class="navigation-bar">
+            <ul class="navigation-bar">
+                <li class="navigation-bar"><a href="home.html" class="Home">Home</a></li>
+                <li class="navigation-bar"><a href="About.php" class="About">About</a></li>
+                <li class="navigation-bar"><a href="Books.php" class="Books">Books</a></li>
+                <li class="navigation-bar"><a href="Contact.php" class="Contact">Contact</a></li>
+                <li class="navigation-bar"><a href="Orders.php" class="Orders">Orders</a></li>
+                <li class="navigation-bar"><a href="FAQ.php" class="FAQ">FAQ</a></li>
+            </ul>
+        </div>    
+    </Header>
+    
+    <!-- Site footer -->
+    <footer class="site-footer">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-10 col-md-4">
+            <img src="images/logo-white.png" alt="logo-white">
+            <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam repellendus sunt praesentium aspernatur iure molestias.</p>
+            <a class social>Learn More About Us</a>
+          </div>  
+          
+          <div class="col-xs-6 col-md-3">
+            <h6>OUR MAIN OFFICE</h6>
+            <ul class="footer-links">
+              <li>Address</li>
+              <li>P.O.BOx:xxx</li>
+              <li>Phone:(+94) xx xxx xxxx</li>
+              <li>E-Mail:info@booklr.com</li>              
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-2">
+            <h6>KEEP IN TOUCH WITH US</h6>
+            <ul class="footer-links">
+              <li><a href="">Facebook</a></li>
+              <li><a href="">Twitter</a></li>
+              <li><a href="">Instergram</a></li>               
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>INFORMATIONS</h6>
+            <table class ="footer-table">
+              <tr>
+                <td>Contact Us</td>
+                <td>Terms & Conditions</td>
+              </tr>
+              <tr>
+                <td>Contact Us</td>
+                <td>My Account</td>
+              </tr>
+              <tr>
+                <td>FAQ</td>
+                <td>Blog</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <hr>
+      </div>
+      
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 col-sm-6 ">
+            <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by <a href="#">Booklr</a>.</p>
+          </div>
+    </footer>
+</html>
